1abd2188be5e421374f1e227e4b414f77aca8751
masilan <robomas@gmail.com>
2014-01-18 16:05:13 -0800

    Added stuff to World.java

--- src/game/World.java
+++ src/game/World.java
@@ -2,4 +2,21 @@ package game;
 
 public class World {
     
+    Chunk[][] chunks = new Chunk[16][16];
+    
+    public void createWorld() {
+        for (int i = 0; i < 16; i++) {
+            for (int j = 0; j < 16; j++) {
+                chunks[i][j] = new Chunk();
+            }
+        }
+    }
+    
+    public void render() {
+        for (int i = 0; i < 16; i++) {
+            for (int j = 0; j < 16; j++) {
+                chunks[i][j].render();
+            }
+        }
+    }
 }
