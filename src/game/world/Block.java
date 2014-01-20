9d8ee25087b526d58a72da5d5f68c16b7a77901b
Shreyas <skraman1999@gmail.com>
2014-01-20 13:54:25 -0800

    Added fake depth perception through legit colors.

--- src/game/world/Block.java
+++ src/game/world/Block.java
@@ -8,103 +8,131 @@ import org.newdawn.slick.opengl.Texture;
 
 public class Block {
     
-    public static final byte DIRT = 0;
+    public static final byte GRASS = 0;
     public static final byte STONE = 1;
     public static final byte BLANK = 2;
     
-    public Texture texture;
-    public boolean isTransparent;
+    private Texture textureTop;
+    private Texture textureSide;
+    private boolean isTransparent;
     private Chunk chunk;
+    private byte type;
+    
+    public boolean isTransparent() { return isTransparent; }
     
     public Block(byte blockName) throws IOException {
-        boolean transparent;
-        String textureName;
+        type = blockName;
         switch (blockName) {
-            case DIRT:
-                transparent = false;
-                textureName = "dirt";
+            case GRASS:
+                isTransparent = false;
+                textureTop = ResourceLibrary.getTexture(ResourceLibrary.GRASS_TOP);
+                textureSide = ResourceLibrary.getTexture(ResourceLibrary.GRASS_SIDE);
                 break;
             case STONE:
-                transparent = false;
-                textureName = "stone_top";
+                isTransparent = false;
+                textureTop = ResourceLibrary.getTexture(ResourceLibrary.STONE_TOP);
+                textureSide = ResourceLibrary.getTexture(ResourceLibrary.STONE_SIDE);
                 break;
             case BLANK:
-                transparent = true;
-                textureName = "blank";
+                isTransparent = true;
+                textureTop = null;
+                textureSide = null;
                 break;
             default:
                 throw new IOException("Unhandled case @ Block constructor.");
         }
-        this.texture = ResourceLibrary.getTextureByName(textureName);
-        this.isTransparent = transparent;
-    } 
+    }
     
     public void setMyChunk(Chunk chunk) {
         this.chunk = chunk;
     }
     
-    private void addFloats(float a, float b, float c, ArrayList<Float> vertices, ArrayList<Float> colorVertices) {
-        vertices.add(Float.valueOf(a));
-        vertices.add(Float.valueOf(b));
-        vertices.add(Float.valueOf(c));
-        colorVertices.add(0f);
-        colorVertices.add(1f);
-        colorVertices.add(0f);
+    private void addFloats(float x, float y, float z, float r, float g, float b,
+            ArrayList<Float> vertices, ArrayList<Float> colorVertices) {
+        vertices.add(Float.valueOf(x));
+        vertices.add(Float.valueOf(y));
+        vertices.add(Float.valueOf(z));
+        colorVertices.add(r);
+        colorVertices.add(g);
+        colorVertices.add(b);
     }
     
     public void render(int x, int y, int z, ArrayList<Float> vertices, ArrayList<Float> colorVertices) {
         if (isTransparent)
             return;
         
-        //texture.bind();
-        // Side.
-        if (!chunk.isBlock(x,y+1,z)) {
-            addFloats(-0.5f+x,-0.5f+z,0.5f+y,vertices,colorVertices);
-            addFloats(0.5f+x,-0.5f+z,0.5f+y,vertices,colorVertices);
-            addFloats(0.5f+x,0.5f+z,0.5f+y,vertices,colorVertices);
-            addFloats(-0.5f+x,0.5f+z,0.5f+y,vertices,colorVertices);
-        }
+        float r = 0;
+        float g = 0;
+        float b = 0;
         
-        // Opposite side to above.
-        if (!chunk.isBlock(x,y-1,z)) {
-            addFloats(-0.5f+x,-0.5f+z,-0.5f+y,vertices,colorVertices);
-            addFloats(-0.5f+x,0.5f+z,-0.5f+y,vertices,colorVertices);
-            addFloats(0.5f+x,0.5f+z,-0.5f+y,vertices,colorVertices);
-            addFloats(0.5f+x,-0.5f+z,-0.5f+y,vertices,colorVertices);
+        if (type == GRASS) {
+            r = 0.09f;
+            g = 0.9f;
+            b = 0.09f;
+        } else if (type == STONE) {
+            r = 0.4f;
+            g = 0.4f;
+            b = 0.4f;
         }
         
         // Top.
         if (!chunk.isBlock(x,y,z+1)) {
-            addFloats(-0.5f+x,0.5f+z,-0.5f+y,vertices,colorVertices);
-            addFloats(-0.5f+x,0.5f+z,0.5f+y,vertices,colorVertices);
-            addFloats(0.5f+x,0.5f+z,0.5f+y,vertices,colorVertices);
-            addFloats(0.5f+x,0.5f+z,-0.5f+y,vertices,colorVertices);
+            addFloats(-0.5f+x,0.5f+z,-0.5f+y,r,g,b,vertices,colorVertices);
+            addFloats(-0.5f+x,0.5f+z,0.5f+y,r,g,b,vertices,colorVertices);
+            addFloats(0.5f+x,0.5f+z,0.5f+y,r,g,b,vertices,colorVertices);
+            addFloats(0.5f+x,0.5f+z,-0.5f+y,r,g,b,vertices,colorVertices);
             GL11.glEnd();
         }
         
-        // Bottom.
-        if (!chunk.isBlock(x,y,z-1)) {
-            addFloats(-0.5f+x,-0.5f+z,-0.5f+y,vertices,colorVertices);
-            addFloats(0.5f+x,-0.5f+z,-0.5f+y,vertices,colorVertices);
-            addFloats(0.5f+x,-0.5f+z,0.5f+y,vertices,colorVertices);
-            addFloats(-0.5f+x,-0.5f+z,0.5f+y,vertices,colorVertices);
+        if (type == GRASS) {
+            r = 0.06f;
+            g = 0.6f;
+            b = 0.06f;
+        } else if (type == STONE) {
+            r = 0.2f;
+            g = 0.2f;
+            b = 0.2f;
+        }
+        
+        // Side.
+        if (!chunk.isBlock(x,y+1,z)) {
+            addFloats(-0.5f+x,-0.5f+z,0.5f+y,r,g,b,vertices,colorVertices);
+            addFloats(0.5f+x,-0.5f+z,0.5f+y,r,g,b,vertices,colorVertices);
+            addFloats(0.5f+x,0.5f+z,0.5f+y,r,g,b,vertices,colorVertices);
+            addFloats(-0.5f+x,0.5f+z,0.5f+y,r,g,b,vertices,colorVertices);
+        }
+        
+        // Opposite side to above.
+        if (!chunk.isBlock(x,y-1,z)) {
+            addFloats(-0.5f+x,-0.5f+z,-0.5f+y,r,g,b,vertices,colorVertices);
+            addFloats(-0.5f+x,0.5f+z,-0.5f+y,r,g,b,vertices,colorVertices);
+            addFloats(0.5f+x,0.5f+z,-0.5f+y,r,g,b,vertices,colorVertices);
+            addFloats(0.5f+x,-0.5f+z,-0.5f+y,r,g,b,vertices,colorVertices);
         }
         
         // Side.
         if (!chunk.isBlock(x+1,y,z)) {
-            addFloats(0.5f+x,-0.5f+z,-0.5f+y,vertices,colorVertices);
-            addFloats(0.5f+x,0.5f+z,-0.5f+y,vertices,colorVertices);
-            addFloats(0.5f+x,0.5f+z,0.5f+y,vertices,colorVertices);
-            addFloats(0.5f+x,-0.5f+z,0.5f+y,vertices,colorVertices);
+            addFloats(0.5f+x,-0.5f+z,-0.5f+y,r,g,b,vertices,colorVertices);
+            addFloats(0.5f+x,0.5f+z,-0.5f+y,r,g,b,vertices,colorVertices);
+            addFloats(0.5f+x,0.5f+z,0.5f+y,r,g,b,vertices,colorVertices);
+            addFloats(0.5f+x,-0.5f+z,0.5f+y,r,g,b,vertices,colorVertices);
             GL11.glEnd();
         }
         
         // Opposite side to above.
         if (!chunk.isBlock(x-1,y,z)) {
-            addFloats(-0.5f+x,-0.5f+z,-0.5f+y,vertices,colorVertices);
-            addFloats(-0.5f+x,-0.5f+z,0.5f+y,vertices,colorVertices);
-            addFloats(-0.5f+x,0.5f+z,0.5f+y,vertices,colorVertices);
-            addFloats(-0.5f+x,0.5f+z,-0.5f+y,vertices,colorVertices);
+            addFloats(-0.5f+x,-0.5f+z,-0.5f+y,r,g,b,vertices,colorVertices);
+            addFloats(-0.5f+x,-0.5f+z,0.5f+y,r,g,b,vertices,colorVertices);
+            addFloats(-0.5f+x,0.5f+z,0.5f+y,r,g,b,vertices,colorVertices);
+            addFloats(-0.5f+x,0.5f+z,-0.5f+y,r,g,b,vertices,colorVertices);
+        }
+        
+        // Bottom.
+        if (!chunk.isBlock(x,y,z-1)) {
+            addFloats(-0.5f+x,-0.5f+z,-0.5f+y,r,g,b,vertices,colorVertices);
+            addFloats(0.5f+x,-0.5f+z,-0.5f+y,r,g,b,vertices,colorVertices);
+            addFloats(0.5f+x,-0.5f+z,0.5f+y,r,g,b,vertices,colorVertices);
+            addFloats(-0.5f+x,-0.5f+z,0.5f+y,r,g,b,vertices,colorVertices);
         }
     }
 }
