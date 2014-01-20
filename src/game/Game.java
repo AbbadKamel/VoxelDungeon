32df9b70d51a01b9cc6588eeba463df676c37f7f
Shreyas <skraman1999@gmail.com>
2014-01-19 21:45:12 -0800

    Formatting; the issue is with perspective. Will fix in testing-testing branch.

--- src/game/Game.java
+++ src/game/Game.java
@@ -15,24 +15,23 @@ public class Game {
     private int VBOColorHandle;
     
     public static void main(String[] args) throws LWJGLException {
-        Game r = new Game();
-        r.Start();
+        Game game = new Game();
+        game.start();
     }
     
-    public void Start() {
+    public void start() {
         try {
-            CreateWindow();
-            InitGL();
-
-            Run();
+            createWindow();
+            init3D();
+            run();
         } catch (Exception e) {
-
             e.printStackTrace();
         }
     }
+    
     DisplayMode displayMode;
 
-    private void CreateWindow() throws Exception {
+    private void createWindow() throws Exception {
         Display.setFullscreen(false);
         DisplayMode d[] = Display.getAvailableDisplayModes();
         for (int i = 0; i < d.length; i++) {
@@ -43,51 +42,47 @@ public class Game {
             }
         }
         Display.setDisplayMode(displayMode);
-        Display.setTitle("LWJGL COMP4");
+        Display.setTitle("COPY-PASTED TUTORIAL");
         Display.create();
     }
 
-    private void InitGL() {
+    private void init3D() {
         GL11.glEnable(GL11.GL_TEXTURE_2D);
         GL11.glShadeModel(GL11.GL_SMOOTH);
-        GL11.glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+        GL11.glClearColor(0.0f,0.0f,0.0f,0.0f);
         GL11.glClearDepth(1.0);
         GL11.glEnable(GL11.GL_DEPTH_TEST);
         GL11.glDepthFunc(GL11.GL_LEQUAL);
+        
         GL11.glEnableClientState(GL11.GL_VERTEX_ARRAY);
-
         GL11.glEnableClientState(GL11.GL_COLOR_ARRAY);
 
         GL11.glMatrixMode(GL11.GL_PROJECTION);
         GL11.glLoadIdentity();
 
-        GLU.gluPerspective(45.0f, (float) displayMode.getWidth()
-                / (float) displayMode.getHeight(), 0.1f, 300.0f);
+        GLU.gluPerspective(45.0f,(float)displayMode.getWidth()/(float)displayMode.getHeight(),0.1f,300.0f);
+        
         GL11.glMatrixMode(GL11.GL_MODELVIEW);
-
-        GL11.glHint(GL11.GL_PERSPECTIVE_CORRECTION_HINT, GL11.GL_NICEST);
+        GL11.glHint(GL11.GL_PERSPECTIVE_CORRECTION_HINT,GL11.GL_NICEST);
     }
 
-    private void Run() {
-        CreateVBO();
+    private void run() {
+        initVBO();
         float RotateYaw = 0;
         while (!Display.isCloseRequested()) {
             try {
-                GL11.glClear(GL11.GL_COLOR_BUFFER_BIT
-                        | GL11.GL_DEPTH_BUFFER_BIT);
+                GL11.glClear(GL11.GL_COLOR_BUFFER_BIT | GL11.GL_DEPTH_BUFFER_BIT);
                 GL11.glLoadIdentity();
 
-                GL11.glTranslatef((float) Math.sin(RotateYaw / 180 * Math.PI), 0f, -4f); // Move Right 1.5 Units And Into
-                // The
-                GL11.glRotatef(45f, 0.4f, 1.0f, 0.1f);
-                GL11.glRotatef(RotateYaw, 1f, 1.0f, 1f);
+                GL11.glTranslatef((float) Math.sin(RotateYaw / 180 * Math.PI),0f,-4f);
+                GL11.glRotatef(45f,0.4f,1.0f,0.1f);
+                GL11.glRotatef(RotateYaw,1f,1.0f,1f);
                 RotateYaw += 2;
                 DrawVBO();
-                // Render();
                 Display.update();
                 Display.sync(60);
             } catch (Exception e) {
-
+                System.out.println(e);
             }
         }
         Display.destroy();
@@ -96,56 +91,54 @@ public class Game {
 
     private void DrawVBO() {
         GL11.glPushMatrix();
-        GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER, VBOVertexHandle);
-        GL11.glVertexPointer(3, GL11.GL_FLOAT, 0, 0L);
-        GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER, VBOColorHandle);
-        GL11.glColorPointer(3, GL11.GL_FLOAT, 0, 0L);
-        GL11.glDrawArrays(GL11.GL_QUADS, 0, 24);
+        GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER,VBOVertexHandle);
+        GL11.glVertexPointer(3,GL11.GL_FLOAT,0,0L);
+        GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER,VBOColorHandle);
+        GL11.glColorPointer(3,GL11.GL_FLOAT,0,0L);
+        GL11.glDrawArrays(GL11.GL_QUADS,0,24);
         GL11.glPopMatrix();
     }
 
-    private void CreateVBO() {
+    private void initVBO() {
         VBOColorHandle = GL15.glGenBuffers();
         VBOVertexHandle = GL15.glGenBuffers();
-        FloatBuffer VertexPositionData = BufferUtils.createFloatBuffer(24 * 3);
+        FloatBuffer VertexPositionData = BufferUtils.createFloatBuffer(24*3);
         VertexPositionData.put(new float[]{
-            1.0f, 1.0f, -1.0f,
-            -1.0f, 1.0f, -1.0f,
-            -1.0f, 1.0f, 1.0f,
-            1.0f, 1.0f, 1.0f,
-            1.0f, -1.0f, 1.0f,
-            -1.0f, -1.0f, 1.0f,
-            -1.0f, -1.0f, -1.0f,
-            1.0f, -1.0f, -1.0f,
-            1.0f, 1.0f, 1.0f,
-            -1.0f, 1.0f, 1.0f,
-            -1.0f, -1.0f, 1.0f,
-            1.0f, -1.0f, 1.0f,
-            1.0f, -1.0f, -1.0f,
-            -1.0f, -1.0f, -1.0f,
-            -1.0f, 1.0f, -1.0f,
-            1.0f, 1.0f, -1.0f,
-            -1.0f, 1.0f, 1.0f,
-            -1.0f, 1.0f, -1.0f,
-            -1.0f, -1.0f, -1.0f,
-            -1.0f, -1.0f, 1.0f,
-            1.0f, 1.0f, -1.0f,
-            1.0f, 1.0f, 1.0f,
-            1.0f, -1.0f, 1.0f,
-            1.0f, -1.0f, -1.0f
+            1.0f,1.0f,-1.0f,
+            -1.0f,1.0f,-1.0f,
+            -1.0f,1.0f,1.0f,
+            1.0f,1.0f,1.0f,
+            1.0f,-1.0f,1.0f,
+            -1.0f,-1.0f,1.0f,
+            -1.0f,-1.0f,-1.0f,
+            1.0f,-1.0f,-1.0f,
+            1.0f,1.0f,1.0f,
+            -1.0f,1.0f,1.0f,
+            -1.0f,-1.0f,1.0f,
+            1.0f,-1.0f,1.0f,
+            1.0f,-1.0f,-1.0f,
+            -1.0f,-1.0f,-1.0f,
+            -1.0f,1.0f,-1.0f,
+            1.0f,1.0f,-1.0f,
+            -1.0f,1.0f,1.0f,
+            -1.0f,1.0f,-1.0f,
+            -1.0f,-1.0f,-1.0f,
+            -1.0f,-1.0f,1.0f,
+            1.0f,1.0f,-1.0f,
+            1.0f,1.0f,1.0f,
+            1.0f,-1.0f,1.0f,
+            1.0f,-1.0f,-1.0f
         });
         VertexPositionData.flip();
-        FloatBuffer VertexColorData = BufferUtils.createFloatBuffer(24 * 3);
-        VertexColorData.put(new float[]{1, 1, 0, 1, 0, 1, 0, 0, 1, 0, 1, 1, 1, 1, 0, 1, 0, 1, 0, 0, 1, 0, 1, 1, 1, 1, 0, 1, 0, 1, 0, 0, 1, 0, 1, 1, 1, 1, 0, 1, 0, 1, 0, 0, 1, 0, 1, 1, 1, 1, 0, 1, 0, 1, 0, 0, 1, 0, 1, 1, 1, 1, 0, 1, 0, 1, 0, 0, 1, 0, 1, 1,});
+        FloatBuffer VertexColorData = BufferUtils.createFloatBuffer(24*3);
+        VertexColorData.put(new float[]{1,1,0,1,0,1,0,0,1,0,1,1,1,1,0,1,0,1,0,0,1,0,1,1,1,1,0,1,0,1,0,0,1,0,1,1,1,1,0,1,0,1,0,0,1,0,1,1,1,1,0,1,0,1,0,0,1,0,1,1,1,1,0,1,0,1,0,0,1,0,1,1,});
         VertexColorData.flip();
 
-        GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER, VBOVertexHandle);
-        GL15.glBufferData(GL15.GL_ARRAY_BUFFER, VertexPositionData,
-                GL15.GL_STATIC_DRAW);
-        GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER, 0);
-        GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER, VBOColorHandle);
-        GL15.glBufferData(GL15.GL_ARRAY_BUFFER, VertexColorData,
-                GL15.GL_STATIC_DRAW);
-        GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER, 0);
+        GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER,VBOVertexHandle);
+        GL15.glBufferData(GL15.GL_ARRAY_BUFFER,VertexPositionData,GL15.GL_STATIC_DRAW);
+        GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER,0);
+        GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER,VBOColorHandle);
+        GL15.glBufferData(GL15.GL_ARRAY_BUFFER,VertexColorData,GL15.GL_STATIC_DRAW);
+        GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER,0);
     }
 }
